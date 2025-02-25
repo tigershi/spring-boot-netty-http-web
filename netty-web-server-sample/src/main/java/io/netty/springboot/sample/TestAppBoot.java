1e8892a372bd608443ed41b71695bff4b5c8388f
Hu Shi <shihu001@chinasoftinc.com>
2025-02-25 16:04:58 +0800

    test sample

--- netty-web-server-sample/src/main/java/io/netty/springboot/sample/TestAppBoot.java
+++ netty-web-server-sample/src/main/java/io/netty/springboot/sample/TestAppBoot.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.ComponentScan;
  *
  */
 @SpringBootApplication
-@ComponentScan
 public class TestAppBoot {
 	public static void main(String[] args) {
 		
