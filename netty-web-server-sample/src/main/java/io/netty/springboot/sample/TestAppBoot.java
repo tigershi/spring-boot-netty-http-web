fcd889725a45274abf34ca8759fece48d66ca9cc
Hu Shi <shihu001@chinasoftinc.com>
2025-02-25 17:52:40 +0800

    add the comment

--- netty-web-server-sample/src/main/java/io/netty/springboot/sample/TestAppBoot.java
+++ netty-web-server-sample/src/main/java/io/netty/springboot/sample/TestAppBoot.java
@@ -2,7 +2,6 @@ package io.netty.springboot.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 /**
  * 
  * @author shihu
